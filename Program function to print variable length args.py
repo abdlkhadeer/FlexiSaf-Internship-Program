e05e1c70f2d2e2f4e4f64c0aabfebdd07bcf515e
abdlkhadeer <78645023+abdlkhadeer@users.noreply.github.com>
2024-06-06 19:42:15 +0100

    Add files via upload

---
+++ Program function to print variable length args.py
@@ -0,0 +1,5 @@
+
+def display_info(**kwargs):
+  for key, value in kwargs.items ():
+      print(f"{key} : {value}")
+display_info(name = "Abdulkadir Mohammed", internship = "Gen AI and DS", level = "Beginner")
